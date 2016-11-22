0069816f0a991d3f37fb099fe4f0b4da415c4cd5
Grigory Evropeytsev <codeman222@gmail.com>
2016-11-22 13:45:29 -0600

    Android version 1.0.4 release

--- AppLimpia/AppLimpia.Droid/Properties/AssemblyInfo.cs
+++ AppLimpia/AppLimpia.Droid/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using Android.Content.PM;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.4")]
+[assembly: AssemblyFileVersion("1.0.4.4")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
