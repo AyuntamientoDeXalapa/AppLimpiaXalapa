3dc1778171157f02749d1f98ea72d6c779df88a4
Grigory Evropeytsev <codeman222@gmail.com>
2016-12-19 19:51:06 -0600

    Version 1.0.7

--- AppLimpia/AppLimpia.Droid/Properties/AssemblyInfo.cs
+++ AppLimpia/AppLimpia.Droid/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using Android.Content.PM;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6.6")]
-[assembly: AssemblyFileVersion("1.0.6.6")]
+[assembly: AssemblyVersion("1.0.7.7")]
+[assembly: AssemblyFileVersion("1.0.7.7")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
--- AppLimpia/AppLimpia/Properties/AssemblyInfo.cs
+++ AppLimpia/AppLimpia/Properties/AssemblyInfo.cs
@@ -28,8 +28,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6.6")]
-[assembly: AssemblyFileVersion("1.0.6.6")]
+[assembly: AssemblyVersion("1.0.7.7")]
+[assembly: AssemblyFileVersion("1.0.7.7")]
 
 // Make internals visible to platform specific projects
 [assembly: InternalsVisibleTo("AppLimpiaiOS")]
